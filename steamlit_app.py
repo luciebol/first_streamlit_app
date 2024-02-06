7dac1b4ba3b614890835d087e604b6df2592d53a
luciebol <123480958+luciebol@users.noreply.github.com>
2024-02-06 12:36:08 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit 
 
 streamlit.title ('My parents New Healthy Diner') 
-streamlit.header(🥣 🥗 'Breakfast Menu')
-streamlit.text(🐔 🥑🍞'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 🐔 'Kale, Spinach & Rocket Smoothie')
-streamlit.text( 🥑🍞'Hard-Boiled Free-Range Egg')
+streamlit.header( '🥣 🥗Breakfast Menu')
+streamlit.text('🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text( '🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
