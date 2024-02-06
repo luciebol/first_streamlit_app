ca102fd18b9857cd2e18b2d572cf048851b7c04e
luciebol <123480958+luciebol@users.noreply.github.com>
2024-02-06 12:32:54 +0100

    Update steamlit_app.py
    update text

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit 
 
 streamlit.title ('My parents New Healthy Diner') 
+streamlit.header ('Breakfast Menu') 
+streamlit.text ('Omega 3 & Blueberry breakfast ')
+streamlit.text ('Kale, Spinach, Rocket Smoothie')
 
 
 
