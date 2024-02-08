728f7271e8b5210c6df80f4d22e129473234c01f
luciebol <123480958+luciebol@users.noreply.github.com>
2024-02-08 13:15:39 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -51,6 +51,9 @@ streamlit.text(my_data_row)
 
 
 select * from pc_rivery_db.public.fruit_load_list
+my_data_row = my_cur.fetchone()
+streamlit.header (" The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
 
 
