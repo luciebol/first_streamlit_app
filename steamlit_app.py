708484d6dfc9ddac3b8e949c1f36eb70ec9235aa
luciebol <123480958+luciebol@users.noreply.github.com>
2024-02-08 16:56:46 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -17,7 +17,6 @@ from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -77,8 +76,21 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-my_data_row = my_cur.fetchall()
 streamlit.header (" The fruit load list contains:")
+#Snowflake-related functions 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button ('Get Fruit Load List'): 
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_row = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+
+
+
 streamlit.dataframe(my_data_rows)
 
 
