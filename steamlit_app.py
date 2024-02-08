20cfbb7f7838b361b9f65d6b9f3bd2be9a5e0d6d
luciebol <123480958+luciebol@users.noreply.github.com>
2024-02-08 16:23:12 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -85,7 +85,7 @@ streamlit.dataframe(my_data_rows)
 streamlit.write ('Thanks for adding ', add_my_fruit)
 my_cur.execute ("insert into fruit_load_list values ('from streamlit')")
 
-streamlit.stop()
+##streamlit.stop()
 
 import snowflake.connector
 
